2ab776cdc486e2345b9673e60df398c15d094250
Angel <Angel@LAPTOP-0CLBP9BE>
2020-03-09 18:07:08 +0100

    Prueba

---
+++ src/javaapplication30/MiniCalculadoraEjemplo.java
@@ -0,0 +1,29 @@
+
+package javaapplication30;
+/**
+ * Clase Calculadora
+ * @author: Angel
+ * @version: version_1.0
+ */
+public class MiniCalculadoraEjemplo {
+
+    /**
+     * hace el numero absoluto de un numero que recibe
+     * @param num Este numero es el que recibe el metodo para hacer el numero absoluto 
+     * @return Devuelve el valor absoluto del numero
+     */
+    public static double valorAbsoluto(double num) {
+        Math.abs(num);
+        return num;
+    }
+    /**
+     * Hace la raiz de un numero que recibe
+     * @param num Este numero es el que recibe el metodo para hacer la raiz cuadrada 
+     * @return Devuelve la raiz cuadrada del numero
+     */
+    public static double raizCuadrada(double num) {
+        Math.sqrt(num);
+        return num;
+    }
+    
+}
